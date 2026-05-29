a9b97c13eee2681f43a130258a9240c9ff04e948
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-29 23:21:32 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,7 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+celsius = float(input("Celsiusdan Fahrenheitga o'tkazish uchun haroratni kiriting: "))
+fahrenheit = celsius_to_fahrenheit(celsius)
+print(f"{celsius}°C {fahrenheit}°F ga teng")
